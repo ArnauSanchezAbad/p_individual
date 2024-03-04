d73d66aea4e40bf75a8933aef56e7b0611cb364a
ArnauSanchezAbad <160512490+ArnauSanchezAbad@users.noreply.github.com>
2024-03-04 17:21:13 +0100

    Practica 2 v1.01
    Pasat a JQuery index.html i menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,15 @@
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
+    $('#play').on('click', function(){
         window.location.assign("./html/game.html");
     });
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
+    $('#options').on('click', function(){
         window.location.assign("./html/options.html");
     });
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
+    $('#saves').on('click', function(){
         console.error("Opció no implementada");
     });
 
-    document.getElementById('exit').addEventListener('click', 
-    function(){
+    $('#exit').on('click', function(){
         console.warn("No es pot sortir!");
     });
-});
